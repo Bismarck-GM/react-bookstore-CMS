72ab2648098774daab1185783768228c8929106a
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-03-12 12:07:13 -0300

    Add reducer for books. Add categories to bookform. Add redux to index.

--- src/components/BooksForm.js
+++ src/components/BooksForm.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
 export default function BooksForm() {
+  const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
   return (
     <div>
       <form action="">
         <input type="text" />
         <input type="text" />
+        <label htmlFor="category">
+          Category:
+          <select id="category">
+            {categories.map(cat => (
+              <option value={cat} key={cat}>{cat}</option>
+            ))}
+          </select>
+        </label>
         <button type="submit">Submit</button>
       </form>
     </div>
