8b59e3ebeeabd94137e1345b39e6f2a7d65a70d4
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-03-12 13:50:41 -0300

    Add state and functions to BookForm

--- src/components/BooksForm.js
+++ src/components/BooksForm.js
@@ -2,20 +2,46 @@ import React from 'react';
 
 export default function BooksForm() {
   const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+  const $select = document.getElementById('category');
+  const [title, setTitle] = React.useState('');
+  const [category, setCategory] = React.useState($select.value);
+  const resetFormFields = () => {
+    const $title = document.getElementById('title');
+    $title.value = '';
+  };
+
+  const handleChange = e => {
+    if (e.target.id === 'title') {
+      setTitle(e.target.value);
+    } else {
+      setCategory(e.target.value);
+    }
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    // createBook({ id: Math.floor(Math.random() * 9999), title, category });
+    console.log(title, category);
+    setTitle('');
+    resetFormFields();
+  };
+
   return (
     <div>
       <form action="">
-        <input type="text" />
-        <input type="text" />
+        <label htmlFor="title">
+          Title:
+          <input id="title" type="text" onChange={handleChange} />
+        </label>
         <label htmlFor="category">
           Category:
-          <select id="category">
+          <select id="category" onChange={handleChange}>
             {categories.map(cat => (
               <option value={cat} key={cat}>{cat}</option>
             ))}
           </select>
         </label>
-        <button type="submit">Submit</button>
+        <button type="submit" onClick={handleSubmit}>Submit</button>
       </form>
     </div>
   );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,30 +1,30 @@
 const mockUpBooks = [
   {
-    id: Math.floor(Math.random() * 100),
+    id: Math.floor(Math.random() * 9999),
     title: 'The old man and the sea',
     category: 'Kids',
 
   },
   {
-    id: Math.floor(Math.random() * 100),
+    id: Math.floor(Math.random() * 9999),
     title: 'The knight in the rusty armor',
     category: 'Action',
 
   },
   {
-    id: Math.floor(Math.random() * 100),
+    id: Math.floor(Math.random() * 9999),
     title: 'Alive',
     category: 'Action',
 
   },
   {
-    id: Math.floor(Math.random() * 100),
+    id: Math.floor(Math.random() * 9999),
     title: 'The Butterfly Effect',
     category: 'Sci-Fi',
 
   },
   {
-    id: Math.floor(Math.random() * 100),
+    id: Math.floor(Math.random() * 9999),
     title: 'Secret weapons of World War II',
     category: 'History',
 
