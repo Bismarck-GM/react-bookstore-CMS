5849d9af2ec3204e554c76ead756af1fc6bd0542
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-03-12 16:33:05 -0300

    Add connection to Redux devtools.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import { createStore } from 'redux';
 import App from './components/App';
 import booksReducer from './reducers/books';
 
-const store = createStore(booksReducer);
+const store = createStore(
+  booksReducer,
+  // eslint-disable-next-line no-underscore-dangle
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 
 ReactDOM.render(
   <Provider store={store}>
