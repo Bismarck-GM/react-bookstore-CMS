8f3d1ab7a35eddad5c79121aec3acd7eda9c85c8
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-03-12 12:52:24 -0300

    Add mock-up state to books reducer.

---
+++ src/reducers/index.js
@@ -0,0 +1,34 @@
+const mockUpBooks = [
+  {
+    id: Math.floor(Math.random() * 100),
+    title: 'The old man and the sea',
+    category: 'Kids',
+
+  },
+  {
+    id: Math.floor(Math.random() * 100),
+    title: 'The knight in the rusty armor',
+    category: 'Action',
+
+  },
+  {
+    id: Math.floor(Math.random() * 100),
+    title: 'Alive',
+    category: 'Action',
+
+  },
+  {
+    id: Math.floor(Math.random() * 100),
+    title: 'The Butterfly Effect',
+    category: 'Sci-Fi',
+
+  },
+  {
+    id: Math.floor(Math.random() * 100),
+    title: 'Secret weapons of World War II',
+    category: 'History',
+
+  },
+];
+
+export default mockUpBooks;
