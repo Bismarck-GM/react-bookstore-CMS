5a05aee681957d3b2095e89eede383adfd60b10b
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-03-15 12:57:54 -0300

    Edit import of reducers. Properly call to combined reducers from Reducers/index

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import App from './components/App';
-import booksReducer from './reducers/books';
+import rootStore from './reducers/index';
 
 const store = createStore(
-  booksReducer,
+  rootStore,
   // eslint-disable-next-line no-underscore-dangle
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
