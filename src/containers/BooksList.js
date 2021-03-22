05a2ebb9e36610a28c46b2982f7a270d3b9621de
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-03-22 13:47:06 -0300

    Add logic to BookList to handle Filter and FilterChanges. Add filter logic.

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -2,15 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Book from '../components/Book';
-import { removeBook } from '../actions';
+import { removeBook, changeFilter } from '../actions';
+import CategoryFilter from '../components/CategoryFilter';
 
-const BooksList = ({ books, removeBook }) => {
+const BooksList = ({
+  books,
+  removeBook,
+  filter,
+  changeFilter,
+}) => {
   const handleRemoveBook = book => {
     removeBook(book);
   };
+  const handleFilterChange = filter => {
+    changeFilter(filter);
+  };
+
+  const filteredBooks = filter.length > 1 ? books : books.filter(
+    book => book.category === filter[0],
+  );
 
   return (
     <div>
+      <CategoryFilter filter={filter} clickHandler={handleFilterChange} />
       <table>
         <thead>
           <tr>
@@ -20,7 +34,9 @@ const BooksList = ({ books, removeBook }) => {
           </tr>
         </thead>
         <tbody>
-          {books.map(book => (<Book book={book} key={book.id} clickHandler={handleRemoveBook} />))}
+          {filteredBooks.map(book => (
+            <Book book={book} key={book.id} clickHandler={handleRemoveBook} />
+          ))}
         </tbody>
       </table>
     </div>
@@ -30,14 +46,18 @@ const BooksList = ({ books, removeBook }) => {
 BooksList.propTypes = {
   books: PropTypes.arrayOf(PropTypes.object).isRequired,
   removeBook: PropTypes.func.isRequired,
+  changeFilter: PropTypes.func.isRequired,
+  filter: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 const mapStateToProps = state => ({
   books: state.books,
+  filter: state.filter,
 });
 
 const mapDispatch = {
   removeBook,
+  changeFilter,
 };
 
 export default connect(mapStateToProps, mapDispatch)(BooksList);
