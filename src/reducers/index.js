fc3e199fc8973117a3083960df5e5071fa713e77
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-03-22 12:43:48 -0300

    Add filter reducer. Add filter reducer to reducer index.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import booksReducer from './books';
+import booksFilter from './filter';
 
 const rootStore = combineReducers({
   books: booksReducer,
+  filter: booksFilter,
 });
 
 export default rootStore;
