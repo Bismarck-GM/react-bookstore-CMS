bf57f85f9ba6d7f8049c3cbe65d8f9c126d8f676
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-04-05 14:00:11 -0300

    Create NavBar component. Edit App.js moved NavBar code to new file.

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import { Flex, Heading } from '@chakra-ui/react';
+import NavBar from './NavBar';
 import BooksList from '../containers/BooksList';
 import BooksForm from '../containers/BooksForm';
 
 export default function App() {
   return (
     <div>
-      <Flex w="100%" h="95px" align="center">
-        <Heading
-          fontFamily="Montserrat"
-          fontSize="30px"
-        >
-          BookStore CMS
-        </Heading>
-        <ul>
-          <li>Books</li>
-          <li>Categories</li>
-        </ul>
-      </Flex>
+      <NavBar />
       <BooksForm />
       <BooksList />
     </div>
---
+++ src/components/NavBar.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {
+  Flex,
+  Heading,
+  Avatar,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+} from '@chakra-ui/react';
+
+export default function NavBar() {
+  return (
+    <Flex w="100%" h="95px" align="center" fontFamily="Montserrat" paddingLeft="5%" paddingRight="5%">
+      <Flex w="50%">
+        <Heading
+          fontSize="30px"
+          color="#0290ff"
+          marginRight="30px"
+          fontWeight="bold"
+        >
+          Bookstore CMS
+        </Heading>
+        <Flex fontSize="13px">
+          <Menu>
+            <MenuButton
+              letterSpacing="1.9px"
+              marginRight="20px"
+              fontWeight="700"
+              paddingTop="5px"
+              cursor="default"
+            >
+              BOOKS
+            </MenuButton>
+          </Menu>
+          <Menu>
+            <MenuButton
+              letterSpacing="1.9px"
+              paddingTop="5px"
+            >
+              CATEGORIES
+            </MenuButton>
+            <MenuList>
+              <MenuItem>Tuvieja</MenuItem>
+            </MenuList>
+          </Menu>
+        </Flex>
+      </Flex>
+      <Flex w="50%" justify="flex-end">
+        <Avatar />
+      </Flex>
+    </Flex>
+  );
+}
